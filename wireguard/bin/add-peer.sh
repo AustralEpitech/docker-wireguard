235abcdecf4d69c43469c67ed9431d3f1aaf5e59
AngeD <ange.duhayon@epitech.eu>
2022-11-06 19:59:38 +0100

    fix: variable name

--- wireguard/bin/add-peer.sh
+++ wireguard/bin/add-peer.sh
@@ -4,10 +4,10 @@
 COUNT="$(grep -c AllowedIPs "$WIREGUARD_CONF")"
 IP="10.0.0.$((COUNT + 2))"
 
-while [ -z "$PUB" ]; do
+while [ -z "$PEER_PUB" ]; do
     PEER_PUB=$(ask 'Peer Public Key: ')
     if ! ask_yn 'Is This correct (yes/No)? '; then
-        unset "ans"
+        unset "PEER_PUB"
         echo
     fi
 done
