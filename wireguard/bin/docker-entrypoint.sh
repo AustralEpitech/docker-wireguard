93c1cf0df8719fb56d6f88e05a99d9c01eab3a47
AngeD <ange.duhayon@epitech.eu>
2022-12-04 15:53:01 +0100

    fix regex

--- wireguard/bin/docker-entrypoint.sh
+++ wireguard/bin/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 . lib.sh
 
 PRIV="$(wg genkey)"
-DEV="$(ip route show | grep default | grep -Po '(?<=dev\s+)\w+')"
+DEV="$(ip route show | grep default | grep -Po '(?<=dev\s)\w+')"
 
-sed -i "s|\$PRIV|$PRIV| s/\$DEV/$DEV/" "$WIREGUARD_CONF"
+sed -i "s|\$PRIV|$PRIV|; s/\$DEV/$DEV/" "$WIREGUARD_CONF"
 chmod 600 "$WIREGUARD_DIR"/*
 
 wg-quick up wg0
